ecb3ddb20afd97f86c7e7a2cab9d31f16c00f87f
Rommill <rommill19@gmail.com>
2026-08-03 22:32:43 +0300

    test(spring): force in-memory H2 database properties in test context

--- backend-spring/src/test/java/com/smarthome/backend_spring/BackendSpringApplicationTests.java
+++ backend-spring/src/test/java/com/smarthome/backend_spring/BackendSpringApplicationTests.java
@@ -2,12 +2,20 @@ package com.smarthome.backend_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class BackendSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
